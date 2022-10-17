5b9c59840aaea5070c01532ba91a1784d6b3f344
ELI DARUSTIYA <114423143+elidarustiya@users.noreply.github.com>
2022-10-17 23:35:39 +0700

    Add files via upload

---
+++ Lagu.java
@@ -0,0 +1,17 @@
+/**
+ * Tugas Sesi 4_Pemprograman Berorientasi Objek
+ * @author ACER_ELI DARUSTIYA_20210040068_TI21A
+ */
+
+public class Lagu {
+    
+    Protected String nama_album; 
+    String nama_album;
+    
+    public Lagu(String nama_album) {
+        this.nama_album = nama_album;
+    }
+    public void infoLagu(){
+        System.out.println("Saya suka mendengarkan lagu");
+    }
+}
